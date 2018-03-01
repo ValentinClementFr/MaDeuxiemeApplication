4476dfb576387516467636ef1f8562f753753825
ValentinClementFr <valentin.clement37@yahoo.fr>
2018-03-01 11:27:01 +0100

    Fonction de gestion d'un age

--- MaDeuxiemeApplication/Program.cs
+++ MaDeuxiemeApplication/Program.cs
@@ -6,9 +6,25 @@ namespace MaDeuxiemeApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Veuillez saisir une phrase et valider avec la touche \"Entrée\"");
-            string saisie = Console.ReadLine();
-            Console.WriteLine("Vous avez saisi : " + saisie);
+            //Console.WriteLine("Veuillez saisir une phrase et valider avec la touche \"Entrée\"");
+            //string saisie = Console.ReadLine();
+            //Console.WriteLine("Vous avez saisi : " + saisie);
+
+            bool ageIsValid = false;
+            int age = -1;
+            while (!ageIsValid)
+            {
+                Console.WriteLine("Veuillez saisir votre age");
+                string saisie = Console.ReadLine();
+                if (int.TryParse(saisie, out age))
+                    ageIsValid = true;
+                else
+                {
+                    ageIsValid = false;
+                    Console.WriteLine("L'age que vous avez saisi est incorrect ...");
+                }
+            }
+            Console.WriteLine("Votre âge est de : " + age);
         }
     }
 }
