fd3abcb74dbbae49914163538fb4c997b296fa54
ValentinClementFr <valentin.clement37@yahoo.fr>
2018-03-02 10:22:07 +0100

    Test deplacement text dans console

--- MaDeuxiemeApplication/Program.cs
+++ MaDeuxiemeApplication/Program.cs
@@ -138,8 +138,53 @@ namespace MaDeuxiemeApplication
             Console.WriteLine("En dessous du premier");
             */
 
+            /*
             CentrerLeTexte("Hello world");
             CentrerLeTexte("Je suis un texte plus long");
+            */
+
+            int i = 0;
+            int j = 0;
+            int largeur = 21;
+            int hauteur = 4;
+            Console.WriteLine(@"      .--------.");
+            Console.WriteLine(@" ____/_____|___ \___");
+            Console.WriteLine(@"O    _   - |   _   ,*");
+            Console.WriteLine(@" '--(_)-------(_)--'");
+            while (true)
+            {
+                ConsoleKeyInfo info = Console.ReadKey(true);
+                switch (info.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (i > 0)
+                        {
+                            Console.MoveBufferArea(i, j, largeur, hauteur, i - 1, j);
+                            i--;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (i < Console.WindowWidth - largeur)
+                        {
+                            Console.MoveBufferArea(i, j, largeur, hauteur, i + 1, j);
+                            i++;
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (j > 0)
+                        {
+                            Console.MoveBufferArea(i, j, largeur, hauteur, i, j - 1);
+                            j--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.MoveBufferArea(i, j, largeur, hauteur, i, j + 1);
+                        j++;
+                        break;
+                }
+                if (info.Key == ConsoleKey.Q)
+                    break;
+            }
         }
 
         private static void CentrerLeTexte(string texte)
